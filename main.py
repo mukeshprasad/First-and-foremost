968fd0ab80b31063bf7487467f5aa369252a577f
mukeshprasad <44356982+mukeshprasad@users.noreply.github.com>
2019-09-28 11:06:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-#return True if sum of any two num in list equals key
+#return True if sum of any two num in list equals key O(n)
 num=[]
-key=25
-m=[]
+key=int(input()) 
 n=int(input())
 for x in range(n):
     num.append(int(input()))
 for x in range(n):
     if key-num[x] in num:
         print(True)
-        break
+        exit()
 print(None)
